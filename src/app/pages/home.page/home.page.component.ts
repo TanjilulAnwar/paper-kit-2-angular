c21f8f3a981479743fe72ee71ecaaec308f45e78
tanjilulanwar <tanjilulanwar@gmail.com>
2023-08-23 20:04:17 +0600

    changes 1.0.1

--- src/app/pages/home.page/home.page.component.ts
+++ src/app/pages/home.page/home.page.component.ts
@@ -8,13 +8,48 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
   focus: any;
   focus1: any;
-model = {
-  headTitle : "Welcome to BBCA",
-  subTitle : "The British bangladeshi Caterers Association",
-}
+  carouselList = [
+    {
+      id: 1,
+      url: "./assets/img/soroush-karimi.jpg",
+      caption: "Somewhere"
+    },
+    {
+      id: 2,
+      url: "./assets/img/federico-beccari.jpg",
+      caption: "Somewhere"
+    },
+    {
+      id: 3,
+      url: "./assets/img/joshua-stannard.jpg",
+      caption: "Somewhere"
+    },
+    {
+      id: 5,
+      url: "./assets/img/joshua-stannard.jpg",
+      caption: "Somewhere"
+    },
+    {
+      id: 6,
+      url: "./assets/img/joshua-stannard.jpg",
+      caption: "Somewhere"
+    },
+    {
+      id: 7,
+      url: "./assets/img/joshua-stannard.jpg",
+      caption: "Somewhere"
+    },
+
+  ]
+  model = {
+    headTitle: "Welcome to BBCA",
+    subTitle: "The British bangladeshi Caterers Association",
+    secondaryHeadTitle: "Catering for Bangladeshi caterers",
+    secondarySubTitle: "We are an industry body set up to represent the best interests of the Bangladeshi-owned and run restaurants, takeaways and allied services providers to the catering sector.",
+  }
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
 
